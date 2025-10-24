36fab3bbb60ec5758eb8b15df6b111a63edc3bcf
Kevin Ghossoub <kevin.ghossoub@gmail.com>
2025-10-23 20:07:19 -0500

    added home page and plate selection page

---
+++ well-plate-selector-code.js
@@ -0,0 +1,4 @@
+const button = document.getElementById("well-plate-selector-confirm-button");
+button.addEventListener('click', function () {
+    window.location.href = "well-selector-page.html";
+}); 
